ed2ae636011d89adada85d6b417f2a4f2a3dda0b
gojincorp <gojincorp@hotmail.com>
2018-09-06 15:45:46 -0700

    Added browserify/babelify to grunt

--- javascript/Gruntfile.js
+++ javascript/Gruntfile.js
@@ -1,5 +1,20 @@
 module.exports = function(grunt) {
-	grunt.registerTask('default', function() {
-		grunt.log.writeln('default grunt task is running...');
+	grunt.initConfig({
+		browserify: {
+			dist: {
+				files: {
+					'Resources/Public/dist/final.js': 'Resources/Public/es6/*.js',
+					'Resources/Private/dist/final.js': 'Resources/Private/es6/*.js'
+				},
+				options: {
+					transform: [['babelify', {presets: "es2015"}]],
+					browserifyOptions: {
+						debug: true
+					}
+				}
+			}
+		}
 	});
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.registerTask('default', ['browserify:dist']);
 };
